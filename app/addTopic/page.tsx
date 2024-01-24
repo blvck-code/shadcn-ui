14842f9a211ce269a4563f92188d10590e21fb7b
maurice <maurice.oluoch@naiparq.co.ke>
2024-01-24 16:57:04 +0300

    latest on adding new topic

--- app/addTopic/page.tsx
+++ app/addTopic/page.tsx
@@ -1,12 +1,54 @@
+"use client";
+
+import { useState } from "react";
+
+import { useRouter } from "next/navigation";
+
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and description is required!");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3001/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic!");
+      }
+    } catch (e) {
+      console.log("Error creating a topic!");
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-3 ">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3 ">
       <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         type="text"
         placeholder="Topic Title"
         className="border border-slate-500 py-2"
       />
       <input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         type="text"
         placeholder="Topic Description"
         className="border border-slate-500 py-2"
